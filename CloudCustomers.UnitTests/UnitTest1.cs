b34ef1d15f87c6f743bc0e4032dfe8ff6070ff1c
gauravthorath <gauravjobs25@gmail.com>
2022-07-06 07:35:02 +0530

    Add project files.

---
+++ CloudCustomers.UnitTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+namespace CloudCustomers.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+
+        ////Parametered Test
+        //[Theory]
+        //[InlineData("foo",1)]
+        //[InlineData("bar",2)]
+        //public void Test2(String input, int bar)
+        //{
+
+        //}
+    }
+}
